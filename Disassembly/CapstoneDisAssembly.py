182d2204d4ff503d1adf21ec2514c0b428797dc1
Seth Arnold <setharn@gmail.com>
2018-10-07 10:42:00 -0400

    Disassembler initial commit
    The initial creation of the disassembler  python script. WIll likely need to be modified later as it currently is formatted to be human readable and the formatting will likely need to be changed.

---
+++ Disassembly/CapstoneDisAssembly.py
@@ -0,0 +1,8 @@
+from capstone import *
+with open("assignment1.srec","rb") as f:
+	CODE = f.read()
+md = Cs(CS_ARCH_ARM, CS_MODE_ARM)
+with open("Assembly.asm", "w") as wf:
+	for i in md.disasm(CODE, 0x0000):
+		print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+		wf.writelines("0x%x:\t%s\t%s \n" %(i.address, i.mnemonic, i.op_str))
