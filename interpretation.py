ed17a6defa08ce5a31804953fd41db7ef07d4c03
Shawn Veliyathu <sveliya1@binghamton.edu>
2018-11-02 01:11:00 -0400

    Assembly file can be parsed in the orignal format
    Edited how each line is stripped to allow the file to be parsed in
    its orignal format. No longer pass " " through .strip(). Resulting in
    changing which indexes to check and writing more newlines to the file.

--- interpretation.py
+++ interpretation.py
@@ -5,28 +5,34 @@ json_file = open("commandsJSON.json")
 json_str = json_file.read()
 json_data = json.loads(json_str)
 
-
-assembly_file = open("assignment1_assembly.txt")
+assembly_file = open("assignment2_assembly.txt", "r")
 interprettedFile = open("parsed.c","w")
 for line in assembly_file:
     if(line == "\n"):
         interprettedFile.write("\n")
     else:
-        splitValues = line.split(" ")
+        splitValues = line.split()
+        if(len(splitValues) == 1):
+           continue
+        # Detect Function Header Format: <Function_Header>
         if( "<" in splitValues[1]):
             labelHeader = splitValues[1].replace("<","")
             labelHeader = labelHeader.replace(">","")
             interprettedFile.write("Label: " + labelHeader)
-        elif(len(splitValues)<4):
+            interprettedFile.write("\n")
+        elif(len(splitValues)<3):
             interprettedFile.write("// could not parse: " + line)
             interprettedFile.write("\n")
-        elif(splitValues[3].upper() in json_data):
-            assemblyToC = json_data.get(splitValues[3].upper())
+        #splitValues[2] contains the instruction
+        elif(splitValues[2].upper() in json_data):
+            assemblyToC = json_data.get(splitValues[2].upper())
             interprettedFile.write(assemblyToC)
             interprettedFile.write("//" + assemblyToC)
-
+            interprettedFile.write("\n")
         else:
             interprettedFile.write("// could not parse: " + line)
+            interprettedFile.write("\n")
 
 interprettedFile.close()
+json_file.close()
 assembly_file.close()
