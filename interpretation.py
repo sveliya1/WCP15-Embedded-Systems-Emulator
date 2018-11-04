cb77583aee3646776f17acb8707be5811b5c88c6
Seth Arnold <setharn@gmail.com>
2018-11-04 15:12:00 -0500

    Updated to write function header to C file
    Mapping small update
    Now adds a main function loop with brackets to the c file.
    Minor formatting and comment changes

--- interpretation.py
+++ interpretation.py
@@ -1,32 +1,32 @@
 import json
 import sys
 
-json_file = open("commandsJSON.json")
-json_str = json_file.read()
-json_data = json.loads(json_str)
-
+json_file = open("commandsJSON.json")   
+json_str = json_file.read()             #convert the file into a string
+json_data = json.loads(json_str)        #convert the string into a dict, by loading the json data.
 
 assembly_file = open("assignment1_assembly.txt")
 interprettedFile = open("parsed.c","w")
+interprettedFile.write("void main(){\ngoto main;")
 for line in assembly_file:
     if(line == "\n"):
         interprettedFile.write("\n")
     else:
         splitValues = line.split(" ")
-        if( "<" in splitValues[1]):
+        if("<" in splitValues[1]):
             toWrite = splitValues[1].replace("<","")
             toWrite = toWrite.replace(">","")
             interprettedFile.write("Label: " + toWrite)
-        elif(len(splitValues)<4):
+        elif(len(splitValues) < 4):
             interprettedFile.write("// could not parse: " + line)
             interprettedFile.write("\n")
         elif(splitValues[3].upper() in json_data):
             toWriteLine = json_data.get(splitValues[3].upper())
-
+            toWriteLine = toWriteLine.replace('Rd',splitValues[4])
             interprettedFile.write(toWriteLine)
             interprettedFile.write("//" + line)
-
         else:
             interprettedFile.write("// could not parse: " + line)
 
+interprettedFile.write("}")
 interprettedFile.close()
